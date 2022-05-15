e70039fdab44a3750172beefccc87eef412e9dd6
rafikkasmi <rafikhdey@gmail.com>
2022-05-15 15:10:11 +0100

    finishing exo 3

--- src/poohomework/exo3/ArbreMot.java
+++ src/poohomework/exo3/ArbreMot.java
@@ -3,9 +3,20 @@ package poohomework.exo3;
 public class ArbreMot {
     Mot node;
     ArbreMot pere,filsG,filsD;
+    //constructeur pour initiliser les racines
+    ArbreMot(Mot mot){
+        this.node=mot;
+        pere=filsD=filsG=null;
+    }
+    //jai utilisé 2 fonctions avec le meme nom dont l'une des 2 est privé , pour pouvoir utilisé la recursivité.
 
+    //maximum cherche le noeud qui se situe a l'extreme droite (vu que l'arbre c'est un ABR)
     Mot maximum(){
-        return new Mot();
+        return maximum(this);
+    }
+    private Mot maximum(ArbreMot arbre){
+        if(arbre.filsD==null) return arbre.node;
+        return maximum(arbre.filsD);
     }
 
     int taille(){
@@ -43,15 +54,14 @@ public class ArbreMot {
             }
         }
         if(temp1.node.compareMots(newMot)==1){
-            temp1.filsG=new ArbreMot();
+            temp1.filsG=new ArbreMot(newMot);
             temp1.filsG.pere=temp1;
-            temp1.filsG.node=newMot;
         }else{
-            temp1.filsD=new ArbreMot();
+            temp1.filsD=new ArbreMot(newMot);
             temp1.filsD.pere=temp1;
-            temp1.filsD.node=newMot;
         }
     }
+    //afficher fait le parcours infixé de l'arbre d'une maniere recursive
     void afficher(){
         afficher(this);
     }
@@ -62,7 +72,6 @@ public class ArbreMot {
             afficher(arbre.filsD);
         }
     }
-
     int profondeur(){
         return profondeur(this);
     }
@@ -77,15 +86,38 @@ public class ArbreMot {
         else
             return (profondeurDroite + 1);
     }
+    //listeValeursAscendants affiche tous les noeuds qui sont des peres d'autres noeuds
     void listeValeursAscendants(){
-
+        listeValeursAscendants(this);
     }
+    private void listeValeursAscendants(ArbreMot arbre){
+        if(arbre!=null){
+            afficher(arbre.filsG);
+            if(arbre.pere!=null)System.out.println(arbre.pere.node.getMt());
+            afficher(arbre.filsD);
+        }
+    }
+    //listeValeursDescendants affiche tous les noeuds qui sont des fils d'autres noeuds
     void listeValeursDescendants(){
-
+        listeValeursDescendants(this);
     }
+    private void listeValeursDescendants(ArbreMot arbre){
+        if(arbre!=null){
+            afficher(arbre.filsG);
+            if(arbre.filsD!=null)System.out.println(arbre.filsD.node.getMt());
+            if(arbre.filsG!=null)System.out.println(arbre.filsG.node.getMt());
+            afficher(arbre.filsD);
+        }
+    }
+    //egale utilise la fonction recherche, et assure l'existence de mot dans l'autre arbre.
     boolean egale(ArbreMot arbre){
-        return identique(arbre);
+        if(arbre==null) return true;
+        if(!this.recherche(arbre.node)){
+            return false;
+        }
+        return true && this.egale(arbre.filsG) && this.egale(arbre.filsD);
     }
+    //identique fait le parcours infixé des 2 arbres en meme temps et assure qu'ils ont la meme valeur
     boolean identique(ArbreMot arbre){
         if(this.taille()!=arbre.taille()) return false;
         return identique(this,arbre);
--- src/poohomework/exo3/Main.java
+++ src/poohomework/exo3/Main.java
@@ -1,34 +1,39 @@
 package poohomework.exo3;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
         TabMot tab=new TabMot(100);
         //ajouter N mots :
-        int N=5;
+        System.out.println("Entrer N le nombre des mots que vous voulez inserer : ");
+        int N=scanner.nextInt();
+        System.out.println("Entrer les mots mot par mot :");
         for (int i = 0; i < N; i++) {
-            tab.ajouterMot(new Mot("rafik"+i));
+            String text=scanner.next();
+            tab.ajouterMot(new Mot(text));
         }
         //afficher les mots qui ont une chaine inferieure a cette chaine :
         final String CHAINE1 = "l'obstination est le chemin de la réussite";
         final Mot MOT1=new Mot(CHAINE1);
-//        System.out.println("Les mots avec une chaine inferieure a `"+CHAINE1+"` sont :");
+        System.out.println("Les mots avec une chaine inferieure a `"+CHAINE1+"` sont :");
         for (int i = 0; i < tab.getCurrent(); i++) {
             if(tab.arrayMots[i].compareMots(MOT1)==-1){
-//                System.out.println(tab.arrayMots[i].getMt());
+                System.out.println(tab.arrayMots[i].getMt());
             }
         }
         //afficher les inverses
         final String CHAINE2="Se réunir est un début ; rester ensemble est un progrès ; travailler ensemble est la réussite.";
         final Mot MOT2=new Mot(CHAINE2);
-//        System.out.println("les inverses des mots qui ont une chaine supérieure à `"+CHAINE2+"` sont :");
+        System.out.println("les inverses des mots qui ont une chaine supérieure à `"+CHAINE2+"` sont :");
         for (int i = 0; i < tab.getCurrent(); i++) {
             if(tab.arrayMots[i].compareMots(MOT2)==1){
-//                System.out.println(tab.arrayMots[i].inverse().getMt());
+                System.out.println(tab.arrayMots[i].inverse().getMt());
             }
         }
 
-        ArbreMot arbre =new ArbreMot();
+        ArbreMot arbre =new ArbreMot(new Mot("test1"));
         arbre.inserer("rafikk");
         arbre.inserer("rafi");
         arbre.inserer("rafik");
@@ -37,17 +42,14 @@ public class Main {
         arbre.inserer("rk");
         arbre.inserer("r");
         //arbre.afficher();
-        ArbreMot arbre2 =new ArbreMot();
-        arbre2.inserer("rafikk");
+        ArbreMot arbre2 =new ArbreMot(new Mot("rafik"));
+        arbre2.inserer("rafikk2");
         arbre2.inserer("rafi");
         arbre2.inserer("rafik");
-        arbre2.inserer("rafikkk");
+        arbre2.inserer("rafikkkfdsafdf");
         arbre2.inserer("rak");
         arbre2.inserer("rk");
-//        arbre2.inserer("r");
-
-//        System.out.println(arbre.recherche(new Mot("rafi")));
-        System.out.println(arbre.egale(arbre2));
+        arbre2.inserer("r");
 
 
 
